67c54ee9cb597ba18a4c1d1703ddae4c62d321ac
Krittin Thirasak <krittin.thirasak@gmail.com>
2024-09-12 22:43:40 +0700

    fix: refactor level maker and brick resources

--- src/Brick.py
+++ src/Brick.py
@@ -1,5 +1,30 @@
 import pygame
+from src.constants import *
 from src.Dependency import *
+
+brick_image_name_list = [
+    "b_blue_1",
+    "b_blue_2",
+    "b_blue_3",
+    "b_blue_4",
+    "b_green_1",
+    "b_green_2",
+    "b_green_3",
+    "b_green_4",
+    "b_red_1",
+    "b_red_2",
+    "b_red_3",
+    "b_red_4",
+    "b_purple_1",
+    "b_purple_2",
+    "b_purple_3",
+    "b_purple_4",
+    "b_orange_1",
+    "b_orange_2",
+    "b_orange_3",
+    "b_orange_4",
+    "b_gray",
+]
 
 class Brick:
     def __init__(self, x, y):
@@ -9,14 +34,15 @@ class Brick:
         self.x=x
         self.y=y
 
-        self.width = 96
-        self.height = 48
+        self.width = BRICK_WIDTH
+        self.height = BRICK_HEIGHT
 
         self.alive = True
         self.rect = pygame.Rect(self.x, self.y, self.width, self.height)
 
     def Hit(self):
         gSounds['brick-hit2'].play()
+        print(f'\nhit brick >> tier: {self.tier} / color: {self.color} / image_idx: {brick_image_name_list[(self.color-1)*4 + self.tier]}')
 
         if self.tier > 0:
             if self.color == 1:
@@ -34,9 +60,15 @@ class Brick:
         if not self.alive:
             gSounds['brick-hit1'].play()
 
+        print(f'          >> tier: {self.tier} / color: {self.color} / image_idx: {brick_image_name_list[(self.color-1)*4 + self.tier]}')
+
+
     def update(self, dt):
         pass
 
     def render(self, screen):
         if self.alive:
-            screen.blit(brick_image_list[((self.color-1)*4)+self.tier], (self.rect.x, self.rect.y))
+            screen.blit(
+                brick_image_list[(self.color-1)*4 + self.tier], 
+                (self.rect.x, self.rect.y)
+            )
--- src/LevelMaker.py
+++ src/LevelMaker.py
@@ -1,5 +1,6 @@
 import random, pygame, math
 from src.Brick import Brick
+from src.constants import *
 
 #patterns
 NONE = 1
@@ -11,6 +12,7 @@ ALTERNATE = 2        # alternative colors
 SKIP = 3             # skip every other brick
 NONE = 4             # no block this row
 
+MAX_COL = 13
 
 class LevelMaker:
     def __init__(self):
@@ -22,7 +24,7 @@ class LevelMaker:
 
         # Not gooood enough!!
         num_rows = random.randint(1, 5)
-        num_cols = random.randint(7, 13)
+        num_cols = random.randint(7, MAX_COL)
         if num_cols % 2 == 0:
             num_cols += 1
         highest_tier = min(3, math.floor(level/5.0))
@@ -44,6 +46,8 @@ class LevelMaker:
             solid_color = random.randint(1, highest_color)
             solid_tier = random.randint(0, highest_tier)
 
+            alternate_tier1, alternate_tier2, solid_tier = 3, 3, 3
+
             for x in range(num_cols):
                 if skip_pattern and skip_flag:
                     skip_flag = not skip_flag
@@ -51,8 +55,10 @@ class LevelMaker:
                 else:
                     skip_flag = not skip_flag
 
-                b = Brick(x*96+24 + (13-num_cols) * 48, y*48)
-                #b = Brick(x * 32 + 8 + (13 - num_cols) * 16, y * 48)
+                b = Brick(
+                    x*BRICK_WIDTH+24 + (MAX_COL-num_cols) * BRICK_HEIGHT, 
+                    y*BRICK_HEIGHT
+                )
 
                 if alternate_pattern and alternate_flag:
                     b.color = alternate_color1
