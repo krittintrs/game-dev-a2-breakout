5c9f5c32bd512515c6f1e89f9193a12cfadf774a
Krittin Thirasak <krittin.thirasak@gmail.com>
2024-09-19 19:32:37 +0700

    fix: refactor brick color index

--- src/Brick.py
+++ src/Brick.py
@@ -29,7 +29,7 @@ brick_image_name_list = [
 class Brick:
     def __init__(self, x, y):
         self.tier=0   #n->0
-        self.color=1  #5->1
+        self.color=0  #4->0
 
         self.x=x
         self.y=y
@@ -42,17 +42,17 @@ class Brick:
 
     def Hit(self):
         gSounds['brick-hit2'].play()
-        print(f'\nhit brick >> tier: {self.tier} / color: {self.color} / image_idx: {brick_image_name_list[(self.color-1)*4 + self.tier]}')
+        # print(f'\nhit brick >> tier: {self.tier} / color: {self.color} / image_idx: {brick_image_name_list[(self.color-1)*4 + self.tier]}')
 
         if self.tier > 0:
-            if self.color == 1:
+            if self.color == 0:
                 self.tier = self.tier - 1
-                self.color = 5
+                self.color = 4
             else:
                 self.color = self.color - 1
 
         else:
-            if self.color == 1:
+            if self.color == 0:
                 self.alive = False
             else:
                 self.color = self.color - 1
@@ -60,7 +60,7 @@ class Brick:
         if not self.alive:
             gSounds['brick-hit1'].play()
 
-        print(f'          >> tier: {self.tier} / color: {self.color} / image_idx: {brick_image_name_list[(self.color-1)*4 + self.tier]}')
+        # print(f'          >> tier: {self.tier} / color: {self.color} / image_idx: {brick_image_name_list[(self.color-1)*4 + self.tier]}')
 
 
     def update(self, dt):
@@ -69,6 +69,6 @@ class Brick:
     def render(self, screen):
         if self.alive:
             screen.blit(
-                brick_image_list[(self.color-1)*4 + self.tier], 
+                brick_image_list[(self.color)*4 + self.tier], 
                 (self.rect.x, self.rect.y)
             )
