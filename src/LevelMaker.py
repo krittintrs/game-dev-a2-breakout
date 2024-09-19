bb1cdf3d58cd66bc33b0e5427dc40384dca2bec2
Krittin Thirasak <krittin.thirasak@gmail.com>
2024-09-19 19:32:54 +0700

    feat: pcg v1

--- src/LevelMaker.py
+++ src/LevelMaker.py
@@ -19,53 +19,114 @@ class LevelMaker:
     def __init__(self):
         pass
 
+    def levelGenerator(level):
+        # Constraints
+
+        STAGE1 = 1
+        STAGE1_MIN_ROW = 1
+        STAGE1_MAX_ROW = 3
+        STAGE1_MIN_COL = 5
+        STAGE1_MAX_COL = 9
+
+        STAGE2 = 7
+        STAGE2_MIN_ROW = 2
+        STAGE2_MAX_ROW = 5
+        STAGE2_MIN_COL = 7
+        STAGE2_MAX_COL = 11
+
+        STAGE3 = 12
+
+        global previous_rows, previous_cols, previous_brick_strength, isPrevSTRUP, isPrevSameSTR
+        
+        if level == STAGE1:
+            num_rows = 1
+            num_cols = 5
+            selected_brick_strength = previous_brick_strength = 0
+            selected_tier, selected_color = derive_tier_and_color(selected_brick_strength)
+            
+            alternate_strength, alternate_tier, alternate_color = selected_brick_strength, selected_tier, selected_color   
+            alternate_pattern = False
+            skip_pattern = False
+            
+            increment = 0
+            
+            isPrevSTRUP = False
+            isPrevSameSTR = True
+
+        elif STAGE1 < level < STAGE3:
+            if STAGE1 < level < STAGE2:
+                MIN_ROW = STAGE1_MIN_ROW
+                MAX_ROW = STAGE1_MAX_ROW
+                MIN_COL = STAGE1_MIN_COL
+                MAX_COL = STAGE1_MAX_COL
+            elif STAGE2 <= level < STAGE3:
+                MIN_ROW = STAGE2_MIN_ROW
+                MAX_ROW = STAGE2_MAX_ROW
+                MIN_COL = STAGE2_MIN_COL
+                MAX_COL = STAGE2_MAX_COL
+
+            selected_brick_strength = (level-1)//2
+            selected_tier, selected_color = derive_tier_and_color(selected_brick_strength)
+            
+            alternate_strength, alternate_tier, alternate_color = selected_brick_strength, selected_tier, selected_color
+                
+            alternate_pattern = False
+            skip_pattern = False
+
+            if selected_brick_strength > previous_brick_strength and not isPrevSTRUP:
+                print('>>> STR UP <<<<')
+                num_rows = min(max(previous_rows + get_increment(level)*-1, MIN_ROW), MAX_ROW)
+                num_cols = min(max(previous_cols + get_increment(level)*2, MIN_COL), MAX_COL) 
+                isPrevSTRUP = True
+                isPrevSameSTR = False
+            else:
+                print('>>> SAME STR - ROW UP <<<<')
+                num_rows = min(previous_rows + 1, MAX_ROW)
+                num_cols = min(max(previous_cols, MIN_COL), MAX_COL) 
+                isPrevSTRUP = False
+                
+                if not isPrevSameSTR:
+                    isPrevSameSTR = True
+                    alternate_strength = selected_brick_strength + 1
+                    alternate_tier, alternate_color = derive_tier_and_color(alternate_strength)
+                    alternate_pattern = True
+                else:
+                    num_cols = min(max(previous_cols + get_increment(level)*2, MIN_COL), STAGE1_MAX_COL) 
+                    skip_pattern = True
+                    
+        # Calculate the number of bricks
+        num_bricks = num_rows * num_cols
+        
+        # Constructing the strength based on color and tier
+        brick_strength = calculate_brick_strength(selected_tier, selected_color)
+        
+        # Calculate difficulty as a function of brick strength and the number of bricks
+        difficulty = brick_strength * num_bricks
+
+        print(f'LEVEL {level}')
+        print(f'dimension: {num_rows} x {num_cols} ({num_bricks})')
+        # print(f'STR: {previous_brick_strength} + {increment}')
+        print(f'SELECTION >>> STR: {selected_brick_strength} (T{selected_tier}-C{selected_color})')
+        print(f'\twith ALTER {alternate_pattern} {alternate_strength}')
+        print(f'\twith SKIP  {skip_pattern}')
+        print(f'Difficulty: {difficulty}\n')
+
+        previous_rows = num_rows
+        previous_cols = num_cols
+        previous_brick_strength = brick_strength
+
+        return num_rows, num_cols, selected_tier, selected_color, alternate_pattern, alternate_tier, alternate_color, skip_pattern
+
     @classmethod
     def CreateMap(cls, level):
         bricks = []
-
-        # Adjust parameters based on the stage (early, middle, late)
-        if level <= 5:
-            # Early Stage
-            num_rows = random.randint(1, 3)
-            num_cols = random.randint(5, 9)
-            lowest_tier = 0
-            highest_tier = 0   
-            lowest_color = 1
-            highest_color = 3 
-        elif 6 <= level <= 10:
-            # Middle Stage
-            num_rows = random.randint(3, 5)
-            num_cols = random.randint(7, 11)
-            lowest_tier = 0
-            highest_tier = random.randint(1, 2)  
-            lowest_color = 2
-            highest_color = 4
-            # unbreakable_chance = 0.1  # 10% chance for unbreakable bricks
-        else:
-            # Late Stage
-            num_rows = random.randint(5, 7)
-            num_cols = random.randint(11, 13)
-            lowest_tier = 2
-            highest_tier = 3
-            lowest_color = 3
-            highest_color = 5
-            # unbreakable_chance = 0.2  # 20% chance for unbreakable bricks
+        
+        num_rows, num_cols, selected_tier, selected_color, alternate_pattern, alternate_tier, alternate_color, skip_pattern = cls.levelGenerator(level)
 
         for y in range(num_rows):
-            skip_pattern = random.choice([True, False])
-            alternate_pattern = random.choice([True, False])
-
-            alternate_color1 = random.randint(lowest_color, highest_color)
-            alternate_color2 = random.randint(lowest_color, highest_color)
-            alternate_tier1 = random.randint(lowest_tier, highest_tier)
-            alternate_tier2 = random.randint(lowest_tier, highest_tier)
 
             skip_flag = random.choice([True, False])
-
             alternate_flag = random.choice([True, False])
-
-            solid_color = random.randint(1, highest_color)
-            solid_tier = random.randint(0, highest_tier)
 
             for x in range(num_cols):
                 if skip_pattern and skip_flag:
@@ -82,17 +143,17 @@ class LevelMaker:
                 )
 
                 if alternate_pattern and alternate_flag:
-                    b.color = alternate_color1
-                    b.tier = alternate_tier1
+                    b.color = alternate_color
+                    b.tier = alternate_tier
                     alternate_flag = not alternate_flag
                 else:
-                    b.color = alternate_color2
-                    b.tier = alternate_tier2
+                    b.color = selected_color
+                    b.tier = selected_tier
                     alternate_flag = not alternate_flag
 
                 if not alternate_pattern:
-                    b.color = solid_color
-                    b.tier = solid_tier
+                    b.color = selected_color
+                    b.tier = selected_tier
 
                 bricks.append(b)
 
@@ -101,3 +162,18 @@ class LevelMaker:
 
         else:
             return bricks
+        
+# Define brick hitpoints based on color and tier
+def calculate_brick_strength(tier, color):
+    return tier * 5 + color  # Tier has more impact than color
+
+def derive_tier_and_color(strength):
+    tier = strength // 5
+    color = (strength % 5)
+    return tier, color
+
+def get_increment(level, initial_one_prob=0.3, increment=0.05, max_prob=0.9):
+    # Calculate the probability of getting 1 based on the level
+    current_one_prob = min(initial_one_prob + increment * (level - 1), max_prob)
+    if level == 1: return 0
+    return 1 if random.random() < current_one_prob else 0
